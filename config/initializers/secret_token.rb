2214f09517993a87e2416dd58b82cc5245d3878f
Michiko HIRATA <m.yodack@gmail.com>
2011-08-10 11:23:57 +0900

    scaffold done!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubySilverExam::Application.config.secret_token = 'a0891ce4f6989b2b00f2548b1fb213fa8b136b8ec303b21ccfc4d6c4de6572893e93b837d9b4aa1e14952559602d6f2ae99afb757aa2739d7573b6ba4912bb64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RubySilverExam::Application.config.session_store :cookie_store, :key => '_ruby_silver_exam_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RubySilverExam::Application.config.session_store :active_record_store
